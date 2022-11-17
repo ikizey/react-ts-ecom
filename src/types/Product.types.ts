7c9bd116a65001701bf611c2b535caffe9d268d1
kizey <ikizey@icloud.com>
2022-11-17 23:29:35 +0400

    fixed ProductType

--- src/types/Product.types.ts
+++ src/types/Product.types.ts
@@ -1,12 +1,7 @@
-type Image = {
-  src: string;
-  alt: string;
-};
-
 export type ProductType = {
   id: string;
   brand: string;
   name: string;
   price: number;
-  images: Image[];
+  imageURL: string;
 };
